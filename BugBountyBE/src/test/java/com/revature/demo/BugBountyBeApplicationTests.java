194d2992d78001f418d5c3e3ef7988b579e746c5
JacobErwin <jacobaerwin@gmail.com>
2020-05-13 15:28:38 -0500

    Spring Boot with dependencies

---
+++ BugBountyBE/src/test/java/com/revature/demo/BugBountyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugBountyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
