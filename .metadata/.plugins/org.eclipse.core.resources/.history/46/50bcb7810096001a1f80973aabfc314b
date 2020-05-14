369027d9c516e0d06ab3c556041917c3124b4918
JacobErwin <jacobaerwin@gmail.com>
2020-05-14 15:53:24 -0500

    More foundation

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/46/50bcb7810096001a1f80973aabfc314b
@@ -0,0 +1,13 @@
+package com.revature.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugBountyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugBountyBeApplication.class, args);
+	}
+
+}
---
+++ BugBountyBE/src/main/java/com/revature/service/BugReportService.java
@@ -0,0 +1,32 @@
+package com.revature.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revature.model.BugReport;
+import com.revature.repository.BugReportRepository;
+
+@Service("bugReportService")
+public class BugReportService {
+
+	@Autowired
+	private BugReportRepository bugReportRepository;
+	
+	public List<BugReport> findAllBugReports(){
+		return this.bugReportRepository.findAll();
+	}
+	
+	public BugReport findById(int id) {
+		return this.bugReportRepository.findById(id);
+	}
+	
+	public void saveBugReport(BugReport bugReport) {
+		this.bugReportRepository.save(bugReport);
+	}
+	
+	public void deleteBugReport(int id) {
+		this.bugReportRepository.deleteById(id);
+	}
+}
