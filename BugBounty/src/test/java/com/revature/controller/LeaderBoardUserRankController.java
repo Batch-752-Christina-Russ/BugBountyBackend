95a05ad1b1e02462d2f89aaf905da22dad6cbdae
jwsntn170 <jwsntn@gmail.com>
2020-05-18 10:24:05 -0500

    added springboottest tag, formating

--- BugBounty/src/test/java/com/revature/controller/LeaderBoardUserRankController.java
+++ BugBounty/src/test/java/com/revature/controller/LeaderBoardUserRankController.java
@@ -17,28 +17,45 @@ import javax.persistence.Column;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.testng.annotations.BeforeClass;
 
 // We will be testing the User-Rank end point of the user controller
+@SpringBootTest
 public class LeaderBoardUserRankController {
 	
 	@Mock
-	private UserService dummy = new UserService();
+	private UserService userService = new UserService();
 	
 	@InjectMocks
 	private UserController testController;
 	
 	private MockMvc mockMVC;
 	
-	Role role = new Role(2, "Admin");
-	User userForTest = new User(5, "bob", "password", 150, role);
 	Object object;
 	
-  @Test
-  public void f() {
+	// the user rank function returns a generic object with these fields
+	class ObjectFromRankAndUser{
+		int points = 150;
+		String username = "bob";
+		int rank = 5;	
+		ObjectFromRankAndUser() {};
+	}
+  
+	@BeforeClass
+	public void beforeClass() {
+		testController = new UserController();
+		MockitoAnnotations.initMocks(this);
+		mockMVC = MockMvcBuilders.standaloneSetup(testController).build();
+		object = new ObjectFromRankAndUser();
+		when(userService.getRankAndUser()).thenReturn(object);	
+	}
+  
+	@Test
+	public void f() {
 		try {
 			mockMVC.perform(get("/user/userrank")).andExpect(status().isOk())
 			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
@@ -46,26 +63,7 @@ public class LeaderBoardUserRankController {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}	
-  }
-  
-  @BeforeClass
-  public void beforeClass() {
-	  testController = new UserController();
-		MockitoAnnotations.initMocks(this);
-		mockMVC = MockMvcBuilders.standaloneSetup(testController).build();
-		object = new ObjectFromRankAndUser();
-		when(dummy.getRankAndUser()).thenReturn(object);	
-  }
-
-  // the user rank function returns a generic object with these fields
-  public class ObjectFromRankAndUser{
-		int points = 150;
-		String username = "bob";
-		int rank = 5;
-		
-		ObjectFromRankAndUser() {};
-  }
-  
+	}
 }
 
 
