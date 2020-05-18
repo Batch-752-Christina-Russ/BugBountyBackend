fa15a263d492ba32dd3cb6cd7325923adc09c0dc
NobleKatana <61984832+NobleKatana@users.noreply.github.com>
2020-05-18 15:06:15 -0700

    merge conflicts

--- BugBounty/src/main/java/com/revature/repository/UserRepository.java
+++ BugBounty/src/main/java/com/revature/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.revature.model.User;
-
+//merge fixin
 @Repository("userRepository")
 public interface UserRepository extends JpaRepository<User, Integer>{
 
