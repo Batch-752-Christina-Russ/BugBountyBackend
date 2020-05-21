977423aa2da9e7fc087da68159c42ab3833615b5
alkinze@gmail.com <alkinze@gmail.com>
2020-05-21 10:41:04 -0500

    added necessary imports to controller

--- BugBounty/src/main/java/com/revature/controller/BugReportController.java
+++ BugBounty/src/main/java/com/revature/controller/BugReportController.java
@@ -1,5 +1,6 @@
 package com.revature.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
