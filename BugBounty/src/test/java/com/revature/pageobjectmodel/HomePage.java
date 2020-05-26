63dcd37df26b4354160f36c2b38f6bb5f2237496
alkinze@gmail.com <alkinze@gmail.com>
2020-05-26 11:57:36 -0500

    added logic to HomePage & ResolveBugPage pom files

--- BugBounty/src/test/java/com/revature/pageobjectmodel/HomePage.java
+++ BugBounty/src/test/java/com/revature/pageobjectmodel/HomePage.java
@@ -1,5 +1,24 @@
 package com.revature.pageobjectmodel;
 
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class HomePage {
 
+	@FindBy(css="table:first-of-type")
+	public WebElement leaderboardTable;
+	
+	@FindBy(css="table:last-of-type")
+	public WebElement userRankRow;
+	
+	@FindBy(className="col-sm-12 col-md-6 col-lg-4")
+	public List<WebElement> openBugs;
+	
+	public HomePage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
 }
